ad24d358a2358b17d238219edec99c7a87fa4425
Ravi Pratap Singh <ravirajput31198@gmail.com>
2024-01-22 15:15:39 +0530

    modified

--- collectionframework/Notes.java
+++ collectionframework/Notes.java
@@ -1,7 +1,23 @@
 package collectionframework;
 
+/* collection frameworks are the groups of object and provide the interfaces and classes for 
+fast manipulation of the data.
+
+There are two types of collections in Java:
+1) Collection Framework (CF): It provides interfaces only, no concrete implementation is provided by JVM.
+2) Util Frameworks (UF): It provides both interface as well as concrete implementations.
+
+Most commonly used collection frameworks are :
+1- ArrayList
+2- LinkedList
+3- HashSet
+4- TreeSet
+5- HashMap
+6- Hashtable
+*/
+
 public class Notes {
     public static void main(String[] args) {
-
+        System.out.println("This is basic information about the collections and collections frameworks.");
     }
 }
