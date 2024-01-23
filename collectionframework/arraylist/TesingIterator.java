16393ae188e7edfc96f72ad2f01f8437a783160d
Ravi Pratap Singh <ravirajput31198@gmail.com>
2024-01-23 11:58:27 +0530

    submit by author

---
+++ collectionframework/arraylist/TesingIterator.java
@@ -0,0 +1,34 @@
+package collectionframework.arraylist;
+
+import java.util.*;
+
+class Testing1 {
+    ArrayList<Integer> thisArr = new ArrayList<>();
+
+    void add() {
+        System.out.println("\nAdd Elements : ");
+        thisArr.add(10);
+        thisArr.add(12);
+        thisArr.add(14);
+        thisArr.add(16);
+        thisArr.add(18);
+        System.out.println(thisArr);
+    }
+
+    void iterate() {
+        System.out.println("\nThis way we use the iterator :");
+        Iterator<Integer> iterator = thisArr.iterator();
+        while (iterator.hasNext()) {
+            System.out.println("The value of next element is : " + iterator.next());
+        }
+
+    }
+}
+
+public class TesingIterator {
+    public static void main(String[] args) {
+        Testing1 t1 = new Testing1();
+        t1.add();
+        t1.iterate();
+    }
+}
