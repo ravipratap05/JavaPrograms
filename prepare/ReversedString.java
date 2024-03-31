a92d8a7e6e1bf04a958758529bd57b7a8584161b
Ravi Pratap Singh <ravirajput31198@gmail.com>
2024-03-31 09:36:34 +0530

    submit

--- prepare/ReversedString.java
+++ prepare/ReversedString.java
@@ -1,5 +1,18 @@
 package prepare;
 
-public class ReversedString {
+// import java.util.ArrayList;
+// import java.util.List;
+import java.util.Scanner;
 
+public class ReversedString {
+    public static void main(String[] args) {
+        System.out.println("Enter the string : ");
+        Scanner s = new Scanner(System.in);
+        String myString = s.nextLine();
+        // System.out.println(myString);
+        StringBuffer sb = new StringBuffer(myString);
+
+        System.out.println(sb.reverse());
+
+    }
 }
